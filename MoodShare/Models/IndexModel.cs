ad5f33247f7bd8201d6bc1c9fd1ebf2ee1f5a403
Andrew Penkrat <contact.aldrog@gmail.com>
2016-05-20 06:07:54 +0300

    Display playlist and audio controls.

--- MoodShare/Models/IndexModel.cs
+++ MoodShare/Models/IndexModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using Mono.Data.Sqlite;
 
 namespace MoodShare
@@ -30,6 +31,36 @@ namespace MoodShare
                 DateTime creationTime = DateTime.FromFileTimeUtc(reader.GetInt64 (3));
                 moods.Add (new Mood { id = id, name = name, author = author, creationTime = creationTime });
             }
+        }
+
+        public struct Track {
+            public int number;
+            public string name;
+            public string performer;
+            public string url;
+        }
+
+        public List<Track> trackList(int id) {
+            List<Track> tl = new List<Track> ();
+            var files = Directory.EnumerateFiles ("Data/" + id + "/music/");
+            foreach (var f in files) {
+                // File name is supposed to look like "N Performer - Song name.mp3"
+                //TODO: get track info from tags
+                string fname = f.Remove (f.LastIndexOf ('.')).Substring(f.LastIndexOf('/') + 1);
+                int n;
+                int.TryParse(fname.Remove (fname.IndexOf (' ')), out n);
+                fname = fname.Substring (fname.IndexOf (' ') + 1);
+                string perf = fname.Remove (fname.IndexOf (" - "));
+                fname = fname.Substring (fname.IndexOf (" - ") + 3);
+                tl.Add (new Track () {
+                    number = n,
+                    name = fname,
+                    performer = perf,
+                    url = f
+                });
+            }
+
+            return tl;
         }
     }
 }
